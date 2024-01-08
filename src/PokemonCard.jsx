74fd57a18a436feaff17297a05b5d5309a0ec156
NASSIM BOUSSAID <NASSIM.BOUSSAID.CSM@gmail.com>
2024-01-08 15:23:17 +0100

    created pokemonCard component

---
+++ src/PokemonCard.jsx
@@ -0,0 +1,14 @@
+function PokemonCard() {
+    return (
+      <figure className="card">
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="bulbasaur"
+          className="card-img"
+        />
+        <figcaption>bulbasaur</figcaption>
+      </figure>
+    );
+  }
+  
+  export default PokemonCard;
