5d688798f0fea1b2685151c8e7baa194e23a363c
nasbous <147812688+nasbous@users.noreply.github.com>
2024-01-22 10:27:19 +0100

    Update PokemonCard.jsx

--- src/PokemonCard.jsx
+++ src/PokemonCard.jsx
@@ -1,20 +1,32 @@
-function PokemonCard({ pokemon }) {
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+function PokemonCard() {
+
+  const pokemon = pokemonList[0];
+
     return (
       <figure className="card">
         {pokemon.imgSrc ? (
-          <img
-            src={pokemon.imgSrc}
-            alt={pokemon.name}
-            className="card-img"
-          />
+        <img
+      src={pokemon.imgSrc}
+      alt={pokemon.name}
+      className="card-img"
+        />
         ) : (
-          <div>
-            <p>????</p>
-          </div>
+          <p>???</p>
         )}
         <figcaption>{pokemon.name}</figcaption>
       </figure>
     );
-  };
-      
-      export default PokemonCard
+  }
+  
+  export default PokemonCard;
